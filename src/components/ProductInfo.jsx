7b9d9023311bcb1a124590a8cf5cd10bb95b60a1
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-25 20:30:26 +0200

    ProductInfo.jsx update

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,7 +1,32 @@
 export default function ProductInfo() {
   return (
-    <article>
-      <h2>ProductInfo goes here</h2>
-    </article>
+    <section>
+      <h1 className="">Sneaker Company</h1>
+
+      <h2 className="">Fall Limited Edition Sneakers</h2>
+
+      <p className="">
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they&apos;ll withstand everything
+        the weather can offer.
+      </p>
+      <div className="">$125.00 50% $250.00</div>
+      <div className="">
+        <div className="">
+          <button>
+            <img src="/icon-minus.svg" alt="" />
+          </button>
+          0
+          <button>
+            <img src="/icon-plus.svg" alt="" />
+          </button>
+        </div>
+        <button className="flex">
+          {" "}
+          <img src="/icon-cart.svg" alt="" />
+          Add to cart
+        </button>
+      </div>
+    </section>
   );
 }
